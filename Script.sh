9b764f7664c49521d786486ca61776084a32e733
AndriiKurapov <andreykawwee@gmail.com>
2021-10-09 18:01:00 +0300

    Removed script file

--- Script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker stop Prod || true
-docker rm Prod || true
-docker rmi docker-tomcat-webapp || true
-docker build -t docker-tomcat-webapp || true
-docker run --name Prod -d -p 8090:8080 -t docker-tomcat-webapp:latest
