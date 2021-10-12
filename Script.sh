9b10fcb31477c990adae0793ae3e8ac635f09093
AndriiKurapov <86830722+AndriiKurapov@users.noreply.github.com>
2021-10-12 18:23:25 +0300

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -2,5 +2,5 @@
 docker stop Prod || true
 docker rm Prod || true
 docker rmi docker-tomcat-webapp || true
-docker build -t docker-tomcat-webapp . || true
+docker build -t docker-tomcat-webapp .
 docker run --name Prod -d -p 8090:8080 -t docker-tomcat-webapp:latest
